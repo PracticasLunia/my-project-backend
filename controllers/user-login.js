c0862fe76711df9c3916fb5034ef4b9869c11d33
Pablo Galvez <pablogalvez31@gmail.com>
2024-07-22 19:18:14 +0200

    JWT & cookie token set

--- controllers/user-login.js
+++ controllers/user-login.js
@@ -6,6 +6,7 @@ export default class UserLoginController {
             const { email, password } = req.body;
             const response = await UserLoginService.login(email, password);
             const token = jwt.sign({ id: response.id }, process.env.JWT_SECRET, { expiresIn: '3h' });
+            res.cookie('token', token, { httpOnly: true, path: '/', maxAge: 3 * 60 * 60 * 1000, secure: false, sameSite: 'strict' });
             res.status(200).json({ user: response, token: token });
         } catch (err){
             res.status(err.status | 400).json({ error: err.message })
--- controllers/user-register.js
+++ controllers/user-register.js
@@ -6,6 +6,7 @@ export default class UserRegisterController {
             const data = req.body;
             const response = await UserRegisterService.register(data);
             const token = jwt.sign({ id: response.id }, process.env.JWT_SECRET, { expiresIn: '3h' });
+            res.cookie('token', token, { httpOnly: true, path: '/', maxAge: 3 * 60 * 60 * 1000, secure: false, sameSite: 'strict' });
             res.status(200).json({ user: response, token: token});
         } catch (err){
             res.status(err.status | 400).json({ error: err.message })
